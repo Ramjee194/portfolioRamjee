c0a5ebaef9234b1a12342816131e2237d8fcef50
Ramjee194 <ramjeekumaryadav733@gmail.com>
2025-09-26 04:16:09 +0530

    sa

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,21 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
+  plugins: [
+    react(),
     tailwindcss(),
   ],
+  // Add these for Render deployment
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    chunkSizeWarningLimit: 1600
+  },
+  preview: {
+    port: 10000,
+    host: true
+  },
+  server: {
+    port: 3000
+  }
 })
